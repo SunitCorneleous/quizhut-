b2292592ea9bf71290fa33935e22bea3a70f3518
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-10-11 00:57:48 +0600

    footer component added

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer footer-center py-12 bg-base-300 text-base-content">
+      <div>
+        <p className="text-2xl">
+          Copyright © 2022 - All right reserved by QuizHut
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
