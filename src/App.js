ef889b848a931ae38cf3c14bfb0cffbc05b93d58
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-10-10 23:50:33 +0600

    context api added for quizzes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { createContext, useEffect, useState } from "react";
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import { router } from "./routes/routes";
 
+export const QuizContext = createContext([]);
+
 function App() {
+  const [topic, setTopics] = useState([]);
+
+  useEffect(() => {
+    fetch("https://openapi.programming-hero.com/api/quiz")
+      .then(res => res.json())
+      .then(data => setTopics(data.data));
+  }, []);
+
   return (
     <>
-      <RouterProvider router={router}></RouterProvider>
+      <QuizContext.Provider value={topic}>
+        <RouterProvider router={router}></RouterProvider>
+      </QuizContext.Provider>
     </>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { QuizContext } from "../App";
 import HeroHeader from "./HeroHeader";
 
 const Home = () => {
+  const topics = useContext(QuizContext);
+  console.log(topics);
   return (
     <div>
       <HeroHeader></HeroHeader>
