feb201dcf11bb244f8c4bca3a34e56bd0dd1c064
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-10-11 01:56:52 +0600

    react tostify added for correct answer

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,17 +1,27 @@
 import React from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Question = ({ data, index }) => {
   const { id, question, options, correctAnswer } = data;
 
+  const notify = value => toast(`Your answer is ${value}`);
+
   const questionCheckHandler = answer => {
-    console.log(answer);
+    if (answer === correctAnswer) {
+      notify("correct");
+    } else {
+      notify("wrong");
+    }
   };
 
   return (
     <div className="p-6 w-11/12 md:w-10/12 mx-auto shadow-xl rounded-lg my-12 border border-1 border-primary">
-      <h2 className="text-center text-xl font-semibold">
-        Quiz {index + 1}: {question}
-      </h2>
+      <div className="w-9/12 mx-auto">
+        <h2 className="text-center text-xl font-semibold">
+          Quiz {index + 1}: {question}
+        </h2>
+      </div>
       <div
         onChange={e => questionCheckHandler(e.target.value)}
         className="grid md:grid-cols-2 my-3 gap-2"
--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import { ToastContainer } from "react-toastify";
 
 const Main = () => {
   return (
     <>
+      <ToastContainer position="top-center" autoClose={2000} />
       <Header></Header>
       <Outlet></Outlet>
       <Footer></Footer>
