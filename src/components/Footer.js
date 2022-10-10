b0f55bbd5f7bfce2377a3b000887e9cf10ec91e3
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-10-11 02:30:51 +0600

    footer font size updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer footer-center py-12 bg-base-300 text-base-content">
       <div>
-        <p className="text-2xl">
+        <p className="text-lg md:text-2xl">
           Copyright © 2022 - All right reserved by QuizHut
         </p>
       </div>
