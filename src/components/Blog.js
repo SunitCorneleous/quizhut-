8b6d548e837661d17f8d70beec09c3dcd6101a85
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-10-11 17:30:37 +0600

    blog page completed

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Blog = () => {
   return (
-    <div className="p-12 min-h-screen md:min-h-[80vh] w-full">
+    <div className="py-12 min-h-screen md:min-h-[80vh] w-full text-center md:text-left">
       <section className="bg-base-100 text-primary-content">
         <div className="container flex flex-col justify-center p-4 mx-auto md:p-8">
           <h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl">
@@ -34,22 +34,30 @@ const Blog = () => {
               </summary>
               <div className="px-4 pb-4">
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-                  aspernatur quae, eos explicabo odit minima libero veniam
-                  similique quibusdam doloribus facilis ipsa accusantium vel
-                  maiores corrupti! Libero voluptate a doloribus?
+                  In react app prop drilling is used to pass data from parent
+                  component to child component. But when there are too many
+                  child components prop drilling gets hard to manage. Context
+                  api is used to avoid the problems of prop drilling. Using
+                  context api props are passed to child components without
+                  drilling in each component. Simply declare a context using
+                  useContext hook then pass the context using context.Provider.
+                  Then the context can be used in the child components using
+                  useContext hook.
                 </p>
               </div>
             </details>
             <details>
               <summary className="py-2 outline-none cursor-pointer focus:underline">
-                3. What is the use of useReff?
+                3. What is the use of useRef?
               </summary>
               <div className="px-4 pb-4 space-y-2">
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
-                  voluptates aspernatur dolores in consequatur doloremque
-                  inventore reprehenderit, consequuntur perspiciatis architecto.
+                  useRef allows to persist values between renders. For example
+                  if we try to count how many times our application renders
+                  using the useState hook we will get caught in a infinite loop
+                  because the hook itself causes a re-render. But if we use
+                  useRef will not face this problem because it will not cause
+                  re-render problem.
                 </p>
               </div>
             </details>
